6158b4174fb4fdb762cdc2f8e409661350839ff0
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-04-23 19:52:11 -0400

    fix

--- Discovery/app/src/main/java/com/example/discovery/Activity/ProfileFragment.java
+++ Discovery/app/src/main/java/com/example/discovery/Activity/ProfileFragment.java
@@ -77,6 +77,6 @@ public class ProfileFragment extends Fragment {
         public View onCreateView (LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState){
             // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_profile, container, false);
+            return inflater.inflate(R.layout.fragment_setting, container, false);
         }
     }
